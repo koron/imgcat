68973deda7d649e2af967abaa380ed99ed636d50
FGtatsuro <fujiistorage@gmail.com>
2014-11-13 20:50:15 +0900

    Add Tiling layout.(But position setting isn't flexible)

--- imgcat.go
+++ imgcat.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+        "math"
 	"image"
 	"image/draw"
 	"image/png"
@@ -15,11 +16,12 @@ type Layout int
 const (
 	Vertical Layout = iota
 	Horizontal
-	// FIXME: support smart "Tiling".
+	Tiling
 )
 
 type Args struct {
 	X, Y, W, H int
+	H_TILE_NUM int
 	Inputs     []string
 	Output     string
 	Layout     Layout
@@ -55,7 +57,8 @@ func flag2args() *Args {
 	flag.IntVar(&a.Y, "y", 0, "Y position, default means top edge")
 	flag.IntVar(&a.W, "width", -1, "width (must)")
 	flag.IntVar(&a.H, "height", -1, "height (must)")
-	flag.IntVar(&layout, "layout", 0, "layout, horz:0 vert:1")
+	flag.IntVar(&layout, "layout", 0, "layout, horz:0 vert:1 tile:2")
+	flag.IntVar(&a.H_TILE_NUM, "h_tile_num", -1, "number of horizon tile (must if and only if layout=Tiling")
 	flag.StringVar(&a.Output, "output", "", "output filename (must)")
 	flag.BoolVar(&a.Help, "h", false, "show help")
 	flag.Parse()
@@ -73,6 +76,9 @@ func args2drawdata(a *Args) []DrawData {
 			x, y = 0, i*a.H
 		case Horizontal:
 			x, y = i*a.W, 0
+		case Tiling:
+			x = (i % a.H_TILE_NUM) * a.W
+			y = (i / a.H_TILE_NUM) * a.H
 		}
 		dd = append(dd, DrawData{
 			File:     s,
@@ -98,6 +104,13 @@ func size(a *Args) image.Rectangle {
 	case Horizontal:
 		w = a.W * len(a.Inputs)
 		h = a.H
+	case Tiling:
+		if len(a.Inputs) < a.H_TILE_NUM {
+			w = a.W
+		} else {
+			w = a.W * a.H_TILE_NUM
+		}
+		h = a.H * int(math.Ceil(float64(len(a.Inputs)) / float64(a.H_TILE_NUM)))
 	}
 	return image.Rect(0, 0, w, h)
 }
@@ -120,6 +133,9 @@ func main() {
 		usage()
 	} else if args.W < 0 || args.H < 0 {
 		fmt.Fprintf(os.Stderr, "required '-width' and '-width'\n\n")
+		usage()
+	} else if args.Layout == 2 && args.H_TILE_NUM < 0 {
+		fmt.Fprintf(os.Stderr, "required '-h_tile_num' when you chose tile layout.\n\n")
 		usage()
 	}
 
