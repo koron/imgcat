2e089e18c5f8809f44b2c8f6c8324b8e098a7377
MURAOKA Taro <koron.kaoriya@gmail.com>
2014-11-14 23:42:00 +0900

    gap for sample bat

--- misc/equip-mini.bat
+++ misc/equip-mini.bat
@@ -1,3 +1,3 @@
 @ECHO OFF
 
-imgcat -x 330 -y 102 -width 228 -height 368 -layout 1 -wrap 3 -output out.png %*
+imgcat -x 330 -y 102 -width 228 -height 368 -layout 1 -wrap 3 -gap 6 -output out.png %*
