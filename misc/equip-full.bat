f70b6ba9cf4f3558484b16d224e8b6e536fe8bb6
MURAOKA Taro <koron.kaoriya@gmail.com>
2014-11-15 00:19:36 +0900

    adjust parameters of example

--- misc/equip-full.bat
+++ misc/equip-full.bat
@@ -1,3 +1,3 @@
 @ECHO OFF
 
-imgcat -x 330 -y 102 -width 454 -height 368 -layout 1 -wrap 3 -gap 6 -output out.png %*
+imgcat -x 330 -y 102 -width 458 -height 366 -layout 1 -wrap 3 -gap 6 -output out.png %*
