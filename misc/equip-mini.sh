340710d1c260393f519aa8eaf384d3bad7d67c1f
MURAOKA Taro <koron.kaoriya@gmail.com>
2015-02-07 01:35:26 +0900

    remove trail slash

--- misc/equip-mini.sh
+++ misc/equip-mini.sh
@@ -8,7 +8,7 @@ layout=1
 wrap=3
 gap=6
 
-dir=$1 ; shift
+dir=${1%/} ; shift
 
 imgcat -x $x -y $y -width $w -height $h -layout $layout -wrap $wrap -gap $gap \
   -output "${dir}.png" ${dir}/*.png
--- misc/orgnaize.sh
+++ misc/orgnaize.sh
@@ -7,7 +7,7 @@ h=368
 layout=0
 gap=6
 
-dir=$1 ; shift
+dir=${1%/} ; shift
 
 imgcat -x $x -y $y -width $w -height $h -layout $layout -gap $gap \
   -output "${dir}.png" ${dir}/*.png
